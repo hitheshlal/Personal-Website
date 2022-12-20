965c31ac80690fcbb733bfdde79e0d682229d2b2
Hithesh lal <hitheshlal.114@gmail.com>
2022-12-20 07:51:23 +0530

    homepage almost completed and started contact page

--- personal.js
+++ personal.js
@@ -1,8 +1,8 @@
-var humburger = document.querySelector(".humb");
+var humburger = document.querySelector(".hump");
 var navlist = document.querySelector(".nav-list");
 var links = document.querySelector(".nav-list li");
 
-hamburger.addEventListner("click" , function(){
+humburger.addEventListener("click" , function(){
     this.classList.toggle("click");
     navlist.classList.toggle("open");
-})
+});
